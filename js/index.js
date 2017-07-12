4b8c316a99899849388a732b12f8136adc9b4437
Josh Kim <josh94kim@berkeley.edu>
2017-07-12 13:41:47 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,121 @@
+'use strict';
+
+var quizQuestions = [
+	{
+	 question: 'How many basketball teams are there in the NBA?',
+	 choices: ['32', '30', '20', '26'],
+	 answer: '1'
+	},
+
+	{
+		question: 'Which NBA team won the most championships?',
+		choices: ['New York Knicks', 'Los Angeles Lakers', 'Chicago Bulls', 'Boston Celtics'],
+		answer: '3'
+	},
+
+	{
+		question: 'Which player holds the all-time record for points scored throughout his NBA career?',
+		choices: ['Kobe Bryant', 'Wilt Chamberlain', 'Michael Jordan', 'Kareem Abdul Jabbar'],
+		answer: '3'
+	},
+
+	{
+		question: 'Which team did Kobe Bryant drop 81 points against?',
+		choices: ['Toronto Raptors', 'Phoenix Suns', 'San Antonio Spurs', 'Boston Celtics'],
+		answer: '0'
+	},
+
+	{
+		question: 'Which team did Michael Jordan retire with?',
+		choices: ['Seattle Supersonics', 'Chicago Bulls', 'Washington Wizards', 'New York Knicks'],
+		answer: '2'
+	},
+
+	{
+		question: 'Which NBA player won the most championships?',
+		choices: ['Kobe Bryant', 'Bill Russell', 'Michael Jordan', 'Kareem Abdul Jabbar'],
+		answer: '1'
+	},
+
+	{
+		question: 'Which player won the finals MVP his rookie season?',
+		choices: ['Mcihael Jordan', 'Magic Johnson', 'Larry Bird', 'Wilt Chamberlain'],
+		answer: '1'
+	},
+
+	{
+		question: 'How many championships has Lebron James won?',
+		choices: ['3', '5', '4', '2'],
+		answer: '0'
+	},
+
+	{
+		question: 'Who was the first player to average a triple double in a season?',
+		choices: ['Oscar Robertson', 'Lebron James', 'Magic Johnson', 'Russel Westbrook'],
+		answer: '0'
+	},
+
+	{
+		question: 'Which one of these players is not a high school draftee? (Attended college and then declared for the draft)',
+		choices: ['Dwight Howard', 'Kevin Garnett', 'Dwyane Wade', 'Kwame Brown']
+		answer: '2'
+	},
+
+	{
+		question: 'Who is the current commissioner of the NBA?',
+		choices: ['Roger Goodell', 'David Stern', 'Rob Manfred', 'Adam Silver'],
+		answer: '3'
+	},
+
+	{
+		question: 'Which player holds the record for most points scored in a game?',
+		choices: ['Michael Jordan', 'Larry Bird', 'Wilt Chamberlain', 'Kobe Bryant'],
+		answer: '2'
+	},
+
+	{
+		question: 'Which of these players is a high school draftee?',
+		choices: ['Tracy Mcgrady', 'Michael Jordan', 'Carmelo Anthony', 'Shaquille O\'Neal'],
+		answer: '0'
+	},
+
+	{
+		question: 'Who is the one and only player to be on the NBA logo?',
+		choices: ['John Havlicek', 'Jerry West', 'Oscar Robertson', 'Pete Maravich'],
+		answer: '1'
+	},
+
+	{
+		question: 'Which player holds the all-time record for assists throughout his NBA career?',
+		choices: ['Steve Nash', 'Jason Kidd', 'Magic Johnson', 'John Stockton'],
+		answer: '3'
+	}
+]
+
+
+function pickQuestion {
+	// picks a question from the array, quizQuestions
+	// Should be in random - Math.floor(Math.random())?
+}
+
+function checkAnswer {
+	// Checks to see if the user's answer evaluates to the correct answer 
+}
+
+function updateQuestionCount {
+	// User should be able to see which question they are on
+}
+
+function updateScore {
+	// User should be able to see their score 
+}
+
+function overallScore {
+	// Users should be able to see their final scores - the questions they got wrong/right
+}
+
+function restartQuiz {
+	// After completing the quiz, user should have the option to restart the quiz 
+}
+
+
